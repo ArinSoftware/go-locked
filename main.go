8a99b8ff3857ab31e90d35ed736c43b7b090f075
gurcancekic <gurcan.cekic@reengen.com>
2022-06-12 15:13:43 +0300

    basic routing

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Welcome to my super site</h1>")
+
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to my super site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "To get in touch email to <a href=\"maitto:test@test.com\">test@test.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>We can't find yhe page you are looking for</h1>")
+	}
+	//fmt.Fprintf(w, "<h1>Welcome to my super site</h1>")
+	//fmt.Fprintf(w, "To get in touch email to <a heref=\"maitto:test@test.com\">test@test.com</a>")
 }
 
 func main() {
